2fe5ab2528aa59fe71c460a89e8b876a2d5ea2b8
madescoces <madescoces@gmail.com>
2024-03-09 12:53:45 -0300

    Project initialization

---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+    },
+  },
+  test: {
+    globals: true,
+    setupFiles: ['./setupTests.ts'], // Separate file for test setup
+    environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html', 'json-summary'],
+    },
+  },
+})
