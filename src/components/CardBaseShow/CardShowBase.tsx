4cc890400d746254ece03d5d485490e82ec1b0d9
JuanCaceffo <caceffojuan@gmail.com>
2024-03-10 21:58:12 -0300

    added first designs for cardShow

--- src/components/CardBaseShow/CardShowBase.tsx
+++ src/components/CardBaseShow/CardShowBase.tsx
@@ -1,36 +1,41 @@
 import { FC } from 'react'
 import './CardShowBase.css'
+import StarIcon from '@mui/icons-material/Star'
 
 const CardShowBase: FC = () => {
   return (
     <>
       <main className="card-show">
         <header className="card-show__header">
-          <img src="" alt="" />
+          <img className="card-show__img" src="/public/mock-imgs/velapuerca.jpg" alt="" />
         </header>
-        <section className="card-show__cont">
-          <header className="card-show__cont--flex">
-            <h6>band name</h6>
-            <div>
-              <i>star icon</i>
-              <b>valoration 0 - 5</b>
-              <span>valoration size</span>
-            </div>
+        <section className="card-show__cont card-show--flex">
+          <header className="card-show--flex">
+            <span className="card-show__fileld">la vela puerca</span>
+            <article className="card-show__fileld">
+              <StarIcon fontSize="0.4em"></StarIcon>
+              <b>4,5</b>
+              <span>(150)</span>
+            </article>
           </header>
-          <article className="card-show__cont">
-            <div>
+          <div className="card-show--flex">
+            <article className="card-show__fileld">
               <b>Ubicacion:</b>
               <span>ubication name</span>
-            </div>
-            <span>function dates</span>
-            <div>
+            </article>
+            <span className="card-show__fileld">function dates</span>
+          </div>
+          <div className="card-show--flex">
+            <article className="card-show__friends card-show--flex">
               <span>Tambien van a asistir</span>
               <div>user imgs</div>
               {/* if it have more that 3 users then put*/}
               <span>+ 3 amigos</span>
-            </div>
-          </article>
-          <footer className="card-show__cont--flex"></footer>
+            </article>
+          </div>
+          <footer className="card-show--flex">
+            <span className="card-show__fileld">desde $$$ a $$$ o precio pagado $$$</span>
+          </footer>
         </section>
       </main>
     </>
