fa906a88dfb62628f69bd885c1443ee16e1bd544
JuanCaceffo <caceffojuan@gmail.com>
2024-03-12 11:32:55 -0300

    added show price or price paied and some improves more

--- src/components/CardBaseShow/CardShowBase.tsx
+++ src/components/CardBaseShow/CardShowBase.tsx
@@ -7,11 +7,7 @@ interface CardShowBaseProps {
   data: Show
 }
 
-const CardShowBase: FC<CardShowBaseProps> = ({ data }) => {
-  const LIMIT_FRIENDS = 3
-
-  const pasedLimitFriends = () => data.userImgs.length > LIMIT_FRIENDS
-  const restFriends = () => pasedLimitFriends() && data.userImgs.length - LIMIT_FRIENDS
+const CardShowBase: FC<CardShowBaseProps> = ({ data: show }) => {
   return (
     <>
       <main className="card-show">
@@ -20,40 +16,44 @@ const CardShowBase: FC<CardShowBaseProps> = ({ data }) => {
         </header>
         <section className="card-show__cont card-show--flex">
           <header className="card-show--flex">
-            <span className="card-show__fileld">{data.name}</span>
+            <span className="card-show__fileld">{show.name}</span>
             <article className="card-show__fileld">
               <StarIcon fontSize="small"></StarIcon>
-              <b>{data.valoration}</b>
-              <span>({data.valorationSize})</span>
+              <b>{show.valoration}</b>
+              <span>({show.valorationSize})</span>
             </article>
           </header>
           <div className="card-show--flex">
             <article className="card-show__fileld">
               <b>Ubicacion:</b>
-              <span>{data.ubication}</span>
+              <span>{show.ubication}</span>
             </article>
             <span className="card-show__fileld">
-              fechas{' '}
-              {data.dates
+              fechas
+              {show.dates
                 .map((date) => new Intl.DateTimeFormat('es-AR', { day: 'numeric', month: 'numeric' }).format(date))
                 .join(' - ')}
             </span>
           </div>
           <div className="card-show--flex">
-            {!!data.userImgs.length && (
+            {!!show.userImgs.length && (
               <article className="card-show__friends card-show--flex">
                 <span>Tambien van a asistir</span>
                 <div className="card-show__user-img-cnt">
-                  {data.userImgs.slice(0, LIMIT_FRIENDS).map((path) => (
+                  {show.userImgs.slice(0, show.LIMIT_FRIENDS).map((path) => (
                     <img className="card-show__user-img" src={path}></img>
                   ))}
                 </div>
-                {pasedLimitFriends() && <span>+ {restFriends()} amigos</span>}
+                {show.pasedLimitFriends() && <span>+ {show.restFriends()} amigos</span>}
               </article>
             )}
           </div>
           <footer className="card-show--flex">
-            <span className="card-show__fileld"></span>
+            <span className="card-show__fileld">
+              {show.wasPricePaid()
+                ? `Precio pagado ${show.price}`
+                : show.pricePaid && `Desde ${show.pricePaid.join(' a ')}`}
+            </span>
           </footer>
         </section>
       </main>
