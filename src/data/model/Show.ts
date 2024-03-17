b842f178b5c9fc55f023789f9a977bf66e65ba6a
JuanCaceffo <caceffojuan@gmail.com>
2024-03-17 00:03:38 -0300

    added interface to show props and modify getLimitedUserImgs method

--- src/data/model/Show.ts
+++ src/data/model/Show.ts
@@ -1,27 +1,18 @@
+import { ShowProps } from '../interfaces/ShowProps'
+
 //TODO: when the imgs managment will finished in the backend change here if is necesary
 export class Show {
-  //TODO: warining with this params (see another wey to do this)
-  constructor(
-    public showImg: string,
-    public name: string,
-    public valoration: number,
-    public valorationSize: number,
-    public ubication: string,
-    public dates: Date[],
-    public userImgs: string[],
-    public price?: number,
-    public pricePaid?: number[],
-  ) {}
+  constructor(public props: ShowProps) {}
   LIMIT_FRIENDS = 3
 
-  getLimitedUserImgs = () => {
-    console.log(this)
-    return this.userImgs.slice(0, this.LIMIT_FRIENDS)
-  }
+  getLimitedUserImgs = () =>
+    this.pasedLimitFriends()
+      ? this.props.userImgs.slice(0, this.LIMIT_FRIENDS)
+      : this.props.userImgs.slice(0, this.props.userImgs.length)
 
-  pasedLimitFriends = () => this.userImgs.length > this.LIMIT_FRIENDS
+  pasedLimitFriends = () => this.props.userImgs.length > this.LIMIT_FRIENDS
 
-  restFriends = () => this.pasedLimitFriends() && this.userImgs.length - this.LIMIT_FRIENDS
+  restFriends = () => this.pasedLimitFriends() && this.props.userImgs.length - this.LIMIT_FRIENDS
 
-  wasPricePaid = () => !!this.price
+  wasPricePaid = () => !!this.props.price
 }
