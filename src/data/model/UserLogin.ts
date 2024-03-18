a9e05991ba1d031e2343843f8ef2c8a66f72e9e2
Denise <deniseamarfil@gmail.com>
2024-03-17 23:35:31 -0300

    Cambio para brobar el build

--- src/data/model/UserLogin.ts
+++ src/data/model/UserLogin.ts
@@ -1,7 +1,7 @@
 export class UserLogin {
   username: string
   password: string
-  
+
   constructor(username: string, password: string) {
     this.username = username
     this.password = password
@@ -11,4 +11,3 @@ export class UserLogin {
     return { ...this }
   }
 }
-  
