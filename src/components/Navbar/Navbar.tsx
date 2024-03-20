b3776ad1c0e10deb9616dcdbf39de0bd783effff
madescoces <madescoces@gmail.com>
2024-03-20 07:54:09 -0300

    Navbar normal funcionando

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import './Navbar.scss'
+import { Link, NavLink } from 'react-router-dom'
+import { Container } from '@mui/material'
+
+export const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false)
+  return (
+    <nav className="navbar">
+      <Link to="/" className="navbar__title">
+        <img src="src/assets/images/logo/logo_minimal.png" alt="Noches Mágicas" />
+      </Link>
+      <Container className="navbar__hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+        <i className="navbar__icon fas fa-bars" />
+      </Container>
+      <ul className={menuOpen ? 'navbar--hamburger' : 'navbar--horizontal'}>
+        <li className="navbar__item">
+          <NavLink className="navbar__link" to="/">
+            Home
+          </NavLink>
+        </li>
+        <li className="navbar__item">
+          <NavLink className="navbar__link" to="/shop">
+            <i className="navbar__icon fas fa-store"></i>
+          </NavLink>
+        </li>
+        <li className="navbar__item">
+          <NavLink className="navbar__link" to="/login">
+            <i className="navbar__icon fas fa-user"></i> Login
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
