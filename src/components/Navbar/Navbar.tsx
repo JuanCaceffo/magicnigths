5e3379f2549d4c581b0856f31dd38ca940a553cc
madescoces <madescoces@gmail.com>
2024-03-20 11:13:50 -0300

    Header y navbar terminado

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -5,16 +5,21 @@ import { Box } from '@mui/material'
 
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <Link to="/" className="navbar__title">
         <img src="src/assets/images/logo/logo_minimal.png" alt="Noches Mágicas" />
       </Link>
-      <Box className="navbar__hamburger" onClick={() => setMenuOpen(!menuOpen)}>
-        {!menuOpen ? <i className="navbar__icon fas fa-bars" /> : ''}
-      </Box>
+      {!menuOpen ? (
+        <Box className="navbar__hamburger" onClick={() => setMenuOpen(!menuOpen)}>
+          <i className="navbar__icon fas fa-bars" />
+        </Box>
+      ) : (
+        <></>
+      )}
       <ul className={`navbar__menu ${menuOpen ? 'navbar__menu--open' : ''}`}>
-        <li className="navbar__item">
+        <li className="navbar__item navbar__item--close">
           <i className="navbar__icon fas fa-xmark" onClick={() => setMenuOpen(false)}></i>
         </li>
         <li className="navbar__item">
