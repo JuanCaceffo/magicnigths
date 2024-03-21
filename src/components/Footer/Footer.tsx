d13d09bd0f515cabd3e9b73a41b8e6c24335b5ea
madescoces <madescoces@gmail.com>
2024-03-21 17:15:56 -0300

    Agregado del año

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,29 +1,27 @@
 import { Container } from '@mui/material'
 import { Logo } from 'src/components/Logo/Logo'
 import { NodeItem } from 'src/components/Navbar/NodeItem'
+import { currentYear } from 'src/data/helpers/getYear'
 
-const buttonClass = 'button button--icon button--shadow'
+const textClass = 'text text--clear text--strong text--spaced shadow--item'
 
 const footerLinks = [
-  { node: <i className="fab fa-facebook" />, link: 'https://www.facebook.com', class: buttonClass },
-  { node: <i className="fab fa-instagram" />, link: 'https://www.instagram.com', class: buttonClass },
-  { node: <i className="fab fa-twitter" />, link: 'https://www.twitter.com', class: buttonClass },
-  { node: <i className="fab fa-linkedin" />, link: 'https://www.linkedin.com', class: buttonClass },
-  { node: <i className="fab fa-at" />, link: 'mailto:nochesmagicas@gmail.com', class: buttonClass },
+  { node: <i className="fab fa-facebook" />, link: 'https://www.facebook.com' },
+  { node: <i className="fab fa-instagram" />, link: 'https://www.instagram.com' },
+  { node: <i className="fab fa-twitter" />, link: 'https://www.twitter.com' },
+  { node: <i className="fab fa-linkedin" />, link: 'https://www.linkedin.com' },
+  { node: <i className="fab fa-at" />, link: 'mailto:nochesmagicas@gmail.com' },
 ]
 
 export const Footer = () => {
   return (
     <Container className="main__footer">
       {footerLinks.map((link) => (
-        <NodeItem
-          key={link.link}
-          node={link.node}
-          link={link.link}
-          className="text text--clear text--strong text--spaced shadow--item"
-        />
+        <NodeItem key={link.link} node={link.node} link={link.link} className={textClass} />
       ))}
       <Logo className="shadow--item" imgUrl={'src/assets/images/logo/logo_minimal.png'} alt={'Noches Mágicas'} />
+      <span className={textClass}>/</span>
+      <span className={textClass}>{currentYear()}</span>
     </Container>
   )
 }
