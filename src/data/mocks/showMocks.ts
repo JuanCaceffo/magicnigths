accbc4f5461f3c04d7d2085c11a7119277a5ced1
JuanCaceffo <caceffojuan@gmail.com>
2024-04-01 01:12:28 -0300

    fix tests

--- src/data/mocks/showMocks.ts
+++ src/data/mocks/showMocks.ts
@@ -2,13 +2,18 @@ import { ShowProps } from '../interfaces/ShowProps'
 import { Show } from '../model/Show'
 
 const baseShowProps: ShowProps = {
-  showImg: '/mock-imgs/card-show-imgs/velapuerca.jpg',
+  id: 1,
+  img: '/mock-imgs/card-show-imgs/velapuerca.jpg',
   name: 'la vela puerca',
-  valoration: 4,
-  valorationSize: 150,
-  ubication: 'Buenos Aires',
-  dates: [new Date('Jul 12 2024'), new Date('Jul 16 2024')],
-  userImgs: ['/mock-imgs/user-imgs/pablito.jpeg', '/mock-imgs/user-imgs/juan.jpeg', '/mock-imgs/user-imgs/denise.jpeg'],
+  rating: 4,
+  totalComments: 150,
+  location: 'Buenos Aires',
+  dates: ['2024-04-11T23:23:34.03791001', '2024-04-12T23:23:34.038422832'],
+  userImageNames: [
+    '/mock-imgs/user-imgs/pablito.jpeg',
+    '/mock-imgs/user-imgs/juan.jpeg',
+    '/mock-imgs/user-imgs/denise.jpeg',
+  ],
   price: 23000,
 }
 
@@ -16,9 +21,9 @@ export const showBase: Show = new Show(baseShowProps)
 export const showUnPaid = new Show({
   ...baseShowProps,
   price: undefined,
-  pricePaid: [23000, 29000],
+  prices: [23000, 29000],
 })
 export const showPassTheFirendLimits: Show = new Show({
   ...baseShowProps,
-  userImgs: [...baseShowProps.userImgs, 'dsadsa'],
+  userImageNames: [...baseShowProps.userImageNames, 'dsadsa'],
 })
