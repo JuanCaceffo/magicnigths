7eeb4ccbdff90168368aa5e45ff91dda093b37f1
JuanCaceffo <caceffojuan@gmail.com>
2024-03-31 23:16:19 -0300

    llamdao al service para popular el arrray de show con datos

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Header } from 'src/components/Header/Header'
 import { Search } from 'src/components/Search/Search'
 import { Page } from 'src/pages/Page/Page'
 import CardShow from 'src/components/CardShow/CardShow'
 import { Show } from 'src/data/model/Show'
+import { showService } from 'src/services/ShowService'
 
 export const Home = () => {
   const [shows, setShows] = useState<Array<Show>>([])
+
+  useEffect(() => {
+    const fechShows = async () => {
+      showService.getShows().then((value) => {
+        setShows(value)
+      })
+    }
+    fechShows()
+  }, [])
 
   return (
     <Page
