5cb7204c67575918c55dd1a0a222cdd377729cd2
JuanCaceffo <caceffojuan@gmail.com>
2024-04-01 01:20:20 -0300

    agregado de manejo de ID de usaurio logeado por sessionStorage

--- src/services/LoginService.tsx
+++ src/services/LoginService.tsx
@@ -1,21 +1,14 @@
 import axios from 'axios'
 import { REST_SERVER_URL } from './contants'
 import { UserLogin } from 'src/data/model/UserLogin'
-
-
+import { USER_KEY_STORAGE } from 'src/data/helpers/userSessionStorage'
 
 class LoginService {
-  id: number
-
-  constructor(){
-    this.id = 0
-  }
-
   async postUserLogin(userLogin: UserLogin) {
-    const idUsuario = axios.post(`${REST_SERVER_URL}/login`,userLogin).then()      
-    this.id = (await idUsuario).data
+    const idUsuario = axios.post(`${REST_SERVER_URL}/login`, userLogin).then()
+    //TODO: buscar una forma mas segura de almacenar el id del usuario logeado
+    sessionStorage.setItem(USER_KEY_STORAGE, (await idUsuario).data)
   }
-
 }
 
 export const loginService = new LoginService()
