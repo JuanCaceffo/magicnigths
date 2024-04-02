78e5023498456bd1aecf2395867b16358122dcb3
madescoces <madescoces@gmail.com>
2024-04-02 13:53:14 -0300

    Vuelvo interfaz a valores originales para sacar el cambio a otra rama

--- src/data/interfaces/ShowProps.ts
+++ src/data/interfaces/ShowProps.ts
@@ -1,16 +1,12 @@
 export interface ShowProps {
-  id: number // showdetails
-  showImg: string // showdetails
-  showName: string // showdetails
-  bandName: string // showdetails
-  facilityName: string // showdetails
-  geoLocation?: string // showdetails
-  rating?: number
-  totalComments?: number
+  id: number
+  img: string
+  name: string
+  location: string
+  rating: number
+  totalComments: number
   price?: number
   prices?: number[]
-  dates: { date: string; soldout: boolean }[] // showdetails
-  userImageNames?: string[]
-  comments?: { imgProfile: string; name: string; date: string; rating: number; text: string }[] // showdetails
-  seats?: { seatType: string; price: number; maxToSell: number }[] // showdetails
+  dates: string[]
+  userImageNames: string[]
 }
