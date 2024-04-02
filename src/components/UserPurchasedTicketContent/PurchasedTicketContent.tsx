f6896cb7589811307fecff223cf15d0a46580674
Denise <deniseamarfil@gmail.com>
2024-04-02 00:57:17 -0300

    Popup comentario y valoración

--- src/components/UserPurchasedTicketContent/PurchasedTicketContent.tsx
+++ src/components/UserPurchasedTicketContent/PurchasedTicketContent.tsx
@@ -1,23 +1,26 @@
+import { useState } from "react"
 import CardShow from "../CardShow/CardShow"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import { userService } from "src/services/UserService"
 import { isAxiosError } from "axios"
 import { Show } from "src/data/model/Show"
-import './PurchasedTicketContent.css'
 
-export const PurchasedTicketContent = (() => {
+import "./PurchasedTicketContent.css"
+import { PopupComment } from "../PopupComment/PopupComment"
 
+export const PurchasedTicketContent = () => {
   const [shows, setShows] = useState<Show[]>([])
-  const [errorMessage, setErrorMessage] = useState('')
-  
+  const [errorMessage, setErrorMessage] = useState("")
+  const [isPopupOpen, setIsPopupOpen] = useState(false) // Estado para controlar si el popup está abierto
+  // const [idShowToComment, setIdShowToComment] = useState(-1)
+
   useEffect(() => {
-    const fetchUserShpws = async () => {
+    const fetchUserShows = async () => {
       try {
-        const userShows= await userService.getPurchasedTickets() //Datos de los shows comprados por el usuario del backend
+        const userShows = await userService.getPurchasedTickets()
         setShows(userShows)
       } catch (e) {
         console.log(e)
-        // Mensaje de error en caso de que lo haya
         if (isAxiosError(e)) {
           if (e.message) {
             setErrorMessage(e.message)
@@ -30,12 +33,25 @@ export const PurchasedTicketContent = (() => {
       }
     }
 
-    fetchUserShpws()
+    fetchUserShows()
   }, [])
 
-  const handleAddComment = () => {
-    //TODO: Hacer funcion
-    console.log("Se apretó el botón")
+  const handleAddComment = (show: Show) => {
+    // setIdShowToComment(show.id) // TODO: agregar atributo al obj de dominio
+    setIsPopupOpen(true) // Abre el popup cuando se hace clic en el botón
+    console.log(show)
+  }
+
+  const handleClosePopup = () => {
+    setIsPopupOpen(false) // Cierra el popup
+  }
+
+  const handleSaveComment = (comment: string) => {
+    // const comment = new Comment() //TODO: Crear objeto de dominio
+    // Lógica para guardar el comentario
+    // userService.addComment()
+    setIsPopupOpen(false) // Cierra el popup después de guardar el comentario
+    console.log(comment)
   }
 
   return (
@@ -43,18 +59,31 @@ export const PurchasedTicketContent = (() => {
       {errorMessage ? (
         <p className="error-message error">{errorMessage}</p>
       ) : (
-        <div className='tickets_container'>
-          {/* Se muestra la card con el botón o sin el botón segun corresponda */}
+        <div className="tickets_container">
           {shows.map((show, index) => (
-            <div>
-              {show.canBeComment() && <CardShow key={index} show={show} button={{ content: "Calificar artista", whenclick: handleAddComment }} />}
-              { !show.canBeComment() && <CardShow key={index} show={show} />}
+            <div key={index}>
+              {show.canBeComment() && (
+                <CardShow
+                  show={show}
+                  button={{
+                    content: "Calificar artista",
+                    whenclick: () => handleAddComment(show),
+                  }}
+                />
+              )}
+              {!show.canBeComment() && <CardShow show={show} />}
             </div>
           ))}
         </div>
       )}
+      {/* Renderiza el popup si está abierto */}
+      {isPopupOpen && (
+        <PopupComment
+          open = {isPopupOpen}
+          onSave={handleSaveComment}
+          onClose={handleClosePopup}
+        />
+      )}
     </>
   )
-  
-
-})
+}
