8e55af5ec81bcfa4850b05c3af0394b9cf1d8a00
madescoces <madescoces@gmail.com>
2024-04-02 14:11:09 -0300

    Correción formateo de fechas

--- src/data/mocks/showMocks.ts
+++ src/data/mocks/showMocks.ts
@@ -7,7 +7,7 @@ const baseShowProps: ShowProps = {
   valoration: 4,
   valorationSize: 150,
   ubication: 'Buenos Aires',
-  dates: [new Date('Jul 12 2024'), new Date('Jul 16 2024')],
+  dates: [new Date('2024-07-12'), new Date('2024-07-16')],
   userImgs: ['/mock-imgs/user-imgs/pablito.jpeg', '/mock-imgs/user-imgs/juan.jpeg', '/mock-imgs/user-imgs/denise.jpeg'],
   price: 23000,
 }
--- src/services/UserService.tsx
+++ src/services/UserService.tsx
@@ -6,23 +6,28 @@ import { Friend } from 'src/data/model/Friend'
 import { Show } from 'src/data/model/Show'
 import { ShowProps } from 'src/data/interfaces/ShowProps'
 
-
-
 class UserService {
   id: number
 
-  constructor(){
+  constructor() {
     this.id = -1
   }
 
   async postUserLogin(userLogin: UserLogin) {
-    const idUsuario = await axios.post(`${REST_SERVER_URL}/login`,userLogin)    
+    const idUsuario = await axios.post(`${REST_SERVER_URL}/login`, userLogin)
     this.id = idUsuario.data
   }
 
   async getUser() {
-    const userData = (await axios.get(`${REST_SERVER_URL}/user_profile/${this.id}`).then()).data    
-    return new User(userData.name, userData.surname, userData.username, new Date(userData.birthday), userData.dni, userData.img)
+    const userData = (await axios.get(`${REST_SERVER_URL}/user_profile/${this.id}`).then()).data
+    return new User(
+      userData.name,
+      userData.surname,
+      userData.username,
+      new Date(userData.birthday),
+      userData.dni,
+      userData.img,
+    )
   }
 
   async updateUser(user: User) {
@@ -32,7 +37,7 @@ class UserService {
       username: user.username,
       birthday: user.birthday.toISOString(), // Fecha a un formato compatible con el servidor
       dni: user.dni,
-      img: user.img
+      img: user.img,
     })
   }
 
@@ -42,7 +47,7 @@ class UserService {
 
   async addCreditToUser(creditToAdd: number) {
     // Actualización del crédito del back
-    await axios.put(`${REST_SERVER_URL}/user_profile/${this.id}/add_credit`, {"credit": creditToAdd})
+    await axios.put(`${REST_SERVER_URL}/user_profile/${this.id}/add_credit`, { credit: creditToAdd })
 
     // Actualización del crédito localmente
     const credit = await this.getCredit()
@@ -56,11 +61,11 @@ class UserService {
     return friends
   }
 
-  async deleteFriend(friendId:number) {
+  async deleteFriend(friendId: number) {
     await axios.delete(`${REST_SERVER_URL}/user_profile/${this.id}/friends/${friendId}`)
   }
 
-  async getPurchasedTickets():  Promise<Show[]> {
+  async getPurchasedTickets(): Promise<Show[]> {
     // const response = await axios.get(`${REST_SERVER_URL}/user_profile/${this.id}/purchased_tickets`)
     // const purchasedTickets = response.data
 
@@ -72,11 +77,15 @@ class UserService {
       valoration: 4,
       valorationSize: 150,
       ubication: 'Buenos Aires',
-      dates: [new Date('Jul 12 2024'), new Date('Jul 16 2024')],
-      userImgs: ['/mock-imgs/user-imgs/pablito.jpeg', '/mock-imgs/user-imgs/juan.jpeg', '/mock-imgs/user-imgs/denise.jpeg'],
-      price: 23000
+      dates: [new Date('2024-07-12'), new Date('2024-07-16')],
+      userImgs: [
+        '/mock-imgs/user-imgs/pablito.jpeg',
+        '/mock-imgs/user-imgs/juan.jpeg',
+        '/mock-imgs/user-imgs/denise.jpeg',
+      ],
+      price: 23000,
     }
-    
+
     const show1: Show = new Show(showProps1)
 
     const showProps2: ShowProps = {
@@ -85,16 +94,15 @@ class UserService {
       valoration: 4,
       valorationSize: 150,
       ubication: 'Buenos Aires',
-      dates: [new Date('Ene 12 2020'), new Date('Jul 16 2024')],
+      dates: [new Date('2020-01-12'), new Date('2020-07-16')],
       userImgs: ['/mock-imgs/user-imgs/pablito.jpeg'],
-      price: 25000
+      price: 25000,
     }
-    
+
     const show2: Show = new Show(showProps2)
 
     return [show1, show2]
   }
 }
-  
 
 export const userService = new UserService()
