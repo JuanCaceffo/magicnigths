94b171a3ec3d9988494f07aa1e758c1fb9c44418
madescoces <madescoces@gmail.com>
2024-04-06 11:50:51 -0300

    Corrección css

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,24 +5,23 @@ import { useState } from 'react'
 import { UserLogin } from 'src/data/model/UserLogin'
 import { userService } from 'src/services/UserService'
 import { useNavigate } from 'react-router-dom'
-import { isAxiosError } from 'axios'
+import { AxiosError } from 'axios'
 import { Input } from '@mui/material'
 import { ErrorHandler } from 'src/error/ErrorHandler'
 
 export const Login = () => {
   const [userLogin, setUser] = useState(new UserLogin('', ''))
   const [errorMessage, setError] = useState<string | null>(null)
-
+  const [text, setText] = useState<string | null>('hola')
   const navigate = useNavigate()
 
   const HandleLoginClick = async () => {
     try {
       await userService.postUserLogin(userLogin)
       navigate('/user_profile')
-    } catch (e) {
-      if (isAxiosError(e)) {
-        ErrorHandler(e, setError)
-      }
+    } catch (err) {
+      setText((err as AxiosError).message)
+      // ErrorHandler(err as AxiosError, setError)
     }
   }
 
@@ -30,25 +29,22 @@ export const Login = () => {
     const { name, value } = event.target
 
     // Se crea un nuevo objeto igual al anterior pero con el nuevo valor del input
-    setUser(prevUser => ({
+    setUser((prevUser) => ({
       ...prevUser,
-      [name]: value
+      [name]: value,
     }))
   }
+
+  setTimeout(() => setText(null), 5000)
 
   return (
     <section className="main__content main__content--login">
       <Card className="card shadow shadow--big">
-        <img className="card__logo" src="/images/logo.png" alt='Noches Magicas' />
+        <img className="card__logo" src="/images/logo.png" alt="Noches Magicas" />
         <form className="card__form" onSubmit={HandleLoginClick}>
           <div className="card__input">
             <label className="text text--light">Usuario</label>
-            <Input
-              className="card__field"
-              name="username"
-              value={userLogin.username}
-              onChange={handleInputChange}
-            />
+            <Input className="card__field" name="username" value={userLogin.username} onChange={handleInputChange} />
           </div>
           <div className="card__input">
             <label className="text text--light">Contraseña</label>
@@ -60,11 +56,11 @@ export const Login = () => {
               onChange={handleInputChange}
             />
           </div>
-          <button className="card__button button shadow--box" type='submit' onClick={HandleLoginClick}>
+          <button className="card__button button shadow--box" type="submit" onClick={HandleLoginClick}>
             Ingresar
           </button>
         </form>
-        {errorMessage && <p className="card__error">{errorMessage}</p>}
+        {errorMessage && <span className="card__error">{errorMessage}</span>}
       </Card>
     </section>
   )
