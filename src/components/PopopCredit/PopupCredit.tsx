e0f023f3420bd9904a8075eca6fcc1864d4b3cef
JuanCaceffo <caceffojuan@gmail.com>
2024-04-06 16:51:59 -0300

    fix boludeses

--- src/components/PopopCredit/PopupCredit.tsx
+++ src/components/PopopCredit/PopupCredit.tsx
@@ -1,7 +1,5 @@
 import { Dialog, DialogActions, DialogContent, InputAdornment, TextField } from '@mui/material'
 import React, { useEffect, useState } from 'react'
-import '../../styles/button.scss'
-import '../../styles/typography.scss'
 import './PopupCredit.css'
 
 export const PopupCredit = ({
@@ -22,14 +20,12 @@ export const PopupCredit = ({
   }, [])
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    if(parseFloat(event.target.value) < 0) {
-      setErrorMessage("El valor no puede ser negativo") 
-    }
-    else {
+    if (parseFloat(event.target.value) < 0) {
+      setErrorMessage('El valor no puede ser negativo')
+    } else {
       setErrorMessage('')
     }
     setCredit(event.target.value)
-    
   }
 
   const handleSave = (event: React.FormEvent<HTMLFormElement>) => {
@@ -52,25 +48,24 @@ export const PopupCredit = ({
             label="Agregar crédito"
             type="number"
             fullWidth
-            InputProps={{ 
+            InputProps={{
               startAdornment: <InputAdornment position="start">$</InputAdornment>,
               inputProps: { min: 0, step: 1 },
             }}
-            value={credit.replace(/^0+/, "")} // Ignora el cero inicial y los ceros a la izquierda
+            value={credit.replace(/^0+/, '')} // Ignora el cero inicial y los ceros a la izquierda
             onChange={handleInputChange}
           />
           {errorMessage && errorMessage}
         </DialogContent>
         <DialogActions>
           <button type="button" className="button button__secondary" onClick={handleClose} color="secondary">
-        Cancelar
+            Cancelar
           </button>
           <button type="submit" className="button" color="primary" disabled={parseFloat(credit) <= 0 || !credit.trim()}>
-        Guardar
+            Guardar
           </button>
         </DialogActions>
       </form>
     </Dialog>
-
   )
 }
